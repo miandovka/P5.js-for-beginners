0a2c190026efe2f603bf87c4ad75740d04e6b95d
Max Frischknecht <max@debutdebut.com>
2019-02-08 13:21:13 +0100

    Audio Frequency Sketch

--- 02_audio/sketch.js
+++ 02_audio/sketch.js
@@ -1,37 +1,41 @@
-let size = 50;
 var mic, fft;
-var bandWidth;
+var numSamples = 128;
 
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	noStroke();
 
 	mic = new p5.AudioIn();
 	mic.start();
-	fft = new p5.FFT(0.9, 128);
+	fft = new p5.FFT(0.9, numSamples);
 	fft.setInput(mic);
-	bandWidth = width / 128;
+
+	colorMode(HSB);
 }
 
 function draw() {
-	background(255);
-	var volume = mic.getLevel();
-	var spectrum = fft.analyze();
-	var value = map(volume, 0, 1, 1, 100);
+	background(0);
 
-	for (var i = 0; i < spectrum.length; i++) {
-		var amp = spectrum[i];
-		var y = map(amp, 0, 255, height, 0);
-		rect(i * bandWidth, y, bandWidth, height - y);
+	var volume = mic.getLevel() * 1000;
+	var spectrum = fft.analyze();
+
+	console.log(volume);
+
+	for(var i = 0; i < spectrum.length; i++) {
+		var x = map(i, 0, spectrum.length - 1, 0, width);
+		var y = map(spectrum[i], 0, 255, 0, height);
+
+		var size = map(spectrum[i], 0, 255, 10, height);
+
+		var c = map(volume, 0, 500, 0, 255);
+		fill(255, c, 255);
+		rectMode(CENTER);
+		rect(x, height/2, 2, size);
 	}
 
-	var bass = spectrum[3];
-	var bassValue = map(bass, 200, 255, 0, 200);
-	ellipse(width/2, height/2, bassValue, bassValue);
+	// fill(0, 0, 255);
+	// ellipse(width/2, height/2, volume, volume);
 
-	// for(let y = 0; y < height; y+=size) {
-	// 	for(let x = 0; x < width; x+=size) {
-	// 	}
-	// }
 
 }
